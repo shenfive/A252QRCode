06d93c12fd9a7788278a2f90d61a56682cfd9e2e
Danny Shen <shenfive@gmail.com>
2025-04-20 15:31:54 +0800

    Initial Commit

---
+++ A252QRCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A252QRCode
+//
+//  Created by 申潤五 on 2025/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
